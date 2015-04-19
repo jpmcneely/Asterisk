1747a231172fcdf0ba54e6d2a1cf5f20ef41d9b1
J McNeely <jm.fwlug@gmail.com>
2015-04-19 08:46:27 -0400

    Split get-pkgs to required and optional versions.

---
+++ get-optional-pkgs.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+apt-get install -y apache2 mysql-server\
+  php5 php5-cli php5-mysql php-pear apache2-utils\
+  phpmyadmin postfix dnsmasq
+
+
--- get-pkgs.sh
+++ get-pkgs.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-apt-get install -y build-essential linux-headers-`uname -r` openssh-server apache2 mysql-server\
-  mysql-client bison flex php5 php5-cli php5-mysql php-pear curl sox apache2-utils binutils-gold\
-  libncurses5-dev libssl-dev libmysqlclient-dev mpg123 mercurial sqlite3 alsa-utils libmyodbc\
+apt-get install -y build-essential linux-headers-`uname -r` openssh-server \
+  mysql-client bison flex curl sox binutils-gold\
+  libncurses5-dev libssl-dev libmysqlclient-dev mpg123 sqlite3 alsa-utils libmyodbc\
   libsqlite3-dev pkg-config automake libtool autoconf git unixodbc-dev uuid uuid-dev oss-compat\
   libasound2-dev libogg-dev libvorbis-dev libsrtp0-dev unixodbc-bin libsox-fmt-all mailutils\
-  libspandsp-dev phpmyadmin postfix dnsmasq libncurses5-dev libxml2-dev libsqlite3-dev sqlite3\
+  libspandsp-dev libncurses5-dev libxml2-dev libsqlite3-dev sqlite3\
   unixodbc-dev libspeex-dev libspeex1 libspeexdsp1 libspeexdsp-dev libcurl4-openssl-dev\
   libiksemel-dev libiksemel-utils libgmime-2.6-dev flite1-dev libflite1 flite
 
